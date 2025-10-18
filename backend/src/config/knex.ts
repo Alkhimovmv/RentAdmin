72a106676b0ca49a02bbfd39af8dbccffee0ce00
alkhimovmv <alkhimovmv@yandex.ru>
2025-10-18 10:58:41 +0300

    шинит

--- backend/src/config/knex.ts
+++ backend/src/config/knex.ts
@@ -3,12 +3,14 @@ require('dotenv').config();
 interface DatabaseConfig {
   client: string;
   connection: {
-    host: string;
-    port: number;
-    database: string;
-    user: string;
-    password: string;
+    host?: string;
+    port?: number;
+    database?: string;
+    user?: string;
+    password?: string;
+    filename?: string;
   };
+  useNullAsDefault?: boolean;
   migrations: {
     directory: string;
   };
@@ -63,22 +65,15 @@ const config: Config = {
   },
 
   production: {
-    client: 'pg',
+    client: 'sqlite3',
     connection: {
-      host: process.env.DB_HOST!,
-      port: parseInt(process.env.DB_PORT!),
-      database: process.env.DB_NAME!,
-      user: process.env.DB_USER!,
-      password: process.env.DB_PASSWORD!,
+      filename: process.env.DB_PATH || './database.sqlite3'
     },
+    useNullAsDefault: true,
     migrations: {
       directory: './src/migrations',
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-  },
+  } as any,
 };
 
 export default config;
